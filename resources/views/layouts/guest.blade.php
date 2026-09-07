2f16579721348107d29ae8d00a09773bd82055e5
Ray Henrique <rayhenrique@gmail.com>
2026-09-07 15:04:32 -0300

    fix(i18n): desativar sugestao de traducao no iOS Safari e fixar pt-BR no manifest e html

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+<html lang="pt-BR" class="h-full notranslate" translate="no">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta http-equiv="content-language" content="pt-BR">
+        <meta name="google" content="notranslate">
 
         <title>{{ config('app.name', 'Caderneta EBD Digital') }}</title>
 
